192dfaf0779c775d4024387ee4a5a490601f986f
StanPri <spribytkovsky@yahoo.com>
2017-02-16 16:36:22 -0800

    Requiring my own files

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 const fs = require('fs');
 const os = require('os');
+const math = require('./math');
 
 let user = os.userInfo();
+let addition = math.add;
 
-fs.appendFile('file.txt', `Hello, ${user.username}. `, function(error)
-  {
-    if(error) {
-      console.log('Unable to write the file');
-    }
-  }
-);
+console.log(`4 + (-1) = ${addition(4,-1)}`)
+
+
+
+// fs.appendFile('file.txt', `Hello, ${user.username}. `, function(error)
+//   {
+//     if(error) {
+//       console.log('Unable to write the file');
+//     }
+//   }
+// );
---
+++ math.js
@@ -0,0 +1,5 @@
+console.log(`Starting message!`);
+
+module.exports.add = (x, y) => {
+  return(x+y);
+}
