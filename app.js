6ab4b813b110aa21e1f5fcae9aaf191ea6c1288c
StanPri <spribytkovsky@yahoo.com>
2017-02-16 16:13:04 -0800

    FileSystem and OS modules added

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-console.log("hello Node.js!");
+const fs = require('fs');
+const os = require('os');
+
+let user = os.userInfo();
+
+fs.appendFile('file.txt', `Hello, ${user.username}. `, function(error)
+  {
+    if(error) {
+      console.log('Unable to write the file');
+    }
+  }
+);
