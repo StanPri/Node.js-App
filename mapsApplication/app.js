49587c861ebb28215c96d12ad46251f7811e6783
StanPri <spribytkovsky@yahoo.com>
2017-02-27 17:48:56 -0800

    Encoding User Input

--- mapsApplication/app.js
+++ mapsApplication/app.js
@@ -1,9 +1,25 @@
 const request = require('request');
+const yargs = require('yargs');
+
+const argv = yargs
+  .options({
+    a: {
+      demand: true,
+      alias: 'address',
+      describe: 'Address to fetch weather for',
+      string: true
+    }
+  }).help()
+  .alias('help', 'h')
+  .argv;
+
+let encodedAddress = encodeURIComponent(argv.address);
 
 request({
-  url: 'https://maps.googleapis.com/maps/api/geocode/json?address=4081%20Tresler%20Ave%20North%20Highlands',
+  url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
   json: true
 }, (error, response, body) => {
-  console.log(body);
-
+  console.log(`Address: ${body.results[0].formatted_address}`);
+  console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
+  console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
 });
