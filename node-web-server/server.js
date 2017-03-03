01e2fc28f96ecdf84a5eab3f5686a7e06b95eed6
StanPri <spribytkovsky@yahoo.com>
2017-03-02 17:59:53 -0800

    Advanced handlebars features added

--- node-web-server/server.js
+++ node-web-server/server.js
@@ -3,14 +3,24 @@ const hbs = require('hbs');
 let app = express();
 let errorMessage = { errorMessage: "Unable to to handle request"};
 
-app.set('view engine', 'hbs');
+
 
 app.use(express.static(__dirname + "/public"));
+
+hbs.registerPartials(__dirname + "/views/partials");
+app.set('view engine', 'hbs');
+
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear()
+});
+
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase();
+});
 
 app.get('/', (req, res) => {
   res.render('index.hbs', {
     pageTitle: 'Home Page',
-    currentYear: new Date().getFullYear(),
     message: "Welcome to this amazing website"
   });
 });
@@ -18,8 +28,7 @@ app.get('/', (req, res) => {
 
 app.get('/about', (req, res) =>{
   res.render('about.hbs', {
-    pageTitle: 'About Page',
-    currentYear: new Date().getFullYear()
+    pageTitle: 'About Page'
   });
 });
 
