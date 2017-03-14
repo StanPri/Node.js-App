feac68255611efdec92903f39441080b0c54a8b4
StanPri <spribytkovsky@yahoo.com>
2017-03-14 09:59:41 -0700

    Data fetch added

--- _node-todo-api/playground/mongodb-find.js
+++ _node-todo-api/playground/mongodb-find.js
@@ -6,18 +6,11 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err,  db) => {
   }
   console.log('Connected to MongoDB server');
 
-
-  db.collection('Users').insertOne({
-    name: 'Maxim',
-    age: 25,
-    location: 'California, US'
-  }, (err, result) => {
-    if(err) {
-      return console.log('Unable to insert Users', err);
+  db.collection('Users').find({name: "Boris"}).toArray().then(
+    (docs) => {
+      console.log(JSON.stringify(docs, undefined, 2));
     }
-
-    console.log(JSON.stringify(result.ops, undefined, 2));
-  });
+  );
 
   db.close();
 });
