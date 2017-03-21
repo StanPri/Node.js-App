2ddcc727b0f973a540183e5a101fa77dfbb9afc2
StanPri <spribytkovsky@yahoo.com>
2017-03-21 10:25:27 -0700

    Changing connection string

--- _node-todo-api/server/db/mongoose.js
+++ _node-todo-api/server/db/mongoose.js
@@ -1,7 +1,7 @@
 let mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise; //Set up third-party library
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
 
 module.exports = {
   mongoose
